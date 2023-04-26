7c712bbe56dcb909b432d6025cc4c20ee94ade44
Noyde <noyde@hotmail.com>
2023-04-26 22:59:42 +0200

    top30

--- oval3ItemsToStreamlit.py
+++ oval3ItemsToStreamlit.py
@@ -24,7 +24,7 @@ dfData = load_data(st.secrets["Oval3DataSource"])
 #st.write(df)
 
 st.write("Top 30 Sell")
-cols = ['Name', 'Att.Rarity','blockchain','lastSale.price', 'lastSale.CurrentUSDPrice', 'lastSale.date','tokenId', 'Att.Club', 'Att.Competition', 'Att.Position', 'Att.Serial_number', 'lastSale.buyer']
+cols = ['Name', 'Att.Rarity', 'blockchain', 'lastSale.price', 'lastSale.CurrentUSDPrice', 'lastSale.date','tokenId', 'Att.Club', 'Att.Competition', 'Att.Position', 'Att.Serial_number', 'lastSale.buyer']
 df = dfData[cols]
 top30_Sell = df.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False)
 
