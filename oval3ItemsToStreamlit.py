1a6f92b054d6f69f4d223d0a0f5e95c673a087a2
Noyde <noyde@hotmail.com>
2023-04-26 22:58:22 +0200

    top30

--- oval3ItemsToStreamlit.py
+++ oval3ItemsToStreamlit.py
@@ -4,9 +4,6 @@ import streamlit as st
 
 ################### Declarations ################
 
-################### Functions ###################
-
-st.write("Welcome to Oval3 Analytics")
 
 
 # Read in data from the Google Sheet.
@@ -16,14 +13,19 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
+
+################### Visualisation ###################
+st.write("Welcome to Oval3 Analytics")
+
+
 dfData = load_data(st.secrets["Oval3DataSource"])
 
 # Print results.
 #st.write(df)
 
 st.write("Top 30 Sell")
-cols = [ 'Name', 'Att.Rarity','blockchain','lastSale.price', 'lastSale.CurrentUSDPrice', 'lastSale.date','tokenId', 'Att.Club', 'Att.Competition', 'Att.Position', 'Att.Serial_number', 'lastSale.buyer']
+cols = ['Name', 'Att.Rarity','blockchain','lastSale.price', 'lastSale.CurrentUSDPrice', 'lastSale.date','tokenId', 'Att.Club', 'Att.Competition', 'Att.Position', 'Att.Serial_number', 'lastSale.buyer']
 df = dfData[cols]
-top30_Sell = (df.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False))
+top30_Sell = df.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False)
 
 st.write(top30_Sell)
