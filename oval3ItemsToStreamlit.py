fd68cd0ae1fd70fe806b234290e8102cdd3cece2
Noyde <noyde@hotmail.com>
2023-04-26 22:30:34 +0200

    test1

--- oval3ItemsToStreamlit.py
+++ oval3ItemsToStreamlit.py
@@ -1,7 +1,5 @@
 
 import pandas as pd
-import requests
-import json
 import streamlit as st
 
 ################### Declarations ################
@@ -9,3 +7,17 @@ import streamlit as st
 ################### Functions ###################
 
 st.write("Hello World!")
+
+
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
