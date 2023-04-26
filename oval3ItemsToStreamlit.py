c72dddd0d06a12209815ca371726730b3e113c29
Noyde <noyde@hotmail.com>
2023-04-26 22:42:03 +0200

    add top 10(2)

--- oval3ItemsToStreamlit.py
+++ oval3ItemsToStreamlit.py
@@ -24,5 +24,5 @@ df = load_data(st.secrets["public_gsheets_url"])
 st.write("Top 10 Sell")
 cols = ['blockchain','lastSale.price', 'lastSale.CurrentUSDPrice', 'lastSale.date' ,'tokenId', 'Name', 'Att.Rarity', 'Att.Club', 'Att.Competition', 'Att.Position', 'Att.Serial_number', 'lastSale.buyer']
 top10_Sell = df.sort_values(by=['lastSale.price'],ascending=False)
-dfTop10Sell = df[cols]
+dfTop10Sell = top10_Sell[cols]
 st.write(dfTop10Sell)
