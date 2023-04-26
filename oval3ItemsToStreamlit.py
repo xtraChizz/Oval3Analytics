4b931ed2e28bbe3568dd84749f9fda8d66d140c5
Noyde <noyde@hotmail.com>
2023-04-26 22:40:37 +0200

    add top 10 sell

--- oval3ItemsToStreamlit.py
+++ oval3ItemsToStreamlit.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 ################### Functions ###################
 
-st.write("Hello World!")
+st.write("Welcome to Oval3 Analytics")
 
 
 # Read in data from the Google Sheet.
@@ -19,4 +19,10 @@ def load_data(sheets_url):
 df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
-st.write(df)
+#st.write(df)
+
+st.write("Top 10 Sell")
+cols = ['blockchain','lastSale.price', 'lastSale.CurrentUSDPrice', 'lastSale.date' ,'tokenId', 'Name', 'Att.Rarity', 'Att.Club', 'Att.Competition', 'Att.Position', 'Att.Serial_number', 'lastSale.buyer']
+top10_Sell = df.sort_values(by=['lastSale.price'],ascending=False)
+dfTop10Sell = df[cols]
+st.write(dfTop10Sell)
