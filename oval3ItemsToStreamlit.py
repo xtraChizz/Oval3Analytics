ac2efe78e6bb4a2287825989551a84744692359c
Noyde <noyde@hotmail.com>
2023-04-26 22:54:47 +0200

    top30

--- oval3ItemsToStreamlit.py
+++ oval3ItemsToStreamlit.py
@@ -16,14 +16,14 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
-df = load_data(st.secrets["public_gsheets_url"])
+dfData = load_data(st.secrets["Oval3DataSource"])
 
 # Print results.
 #st.write(df)
 
 st.write("Top 30 Sell")
-cols = ['blockchain','lastSale.price', 'lastSale.CurrentUSDPrice', 'lastSale.date' ,'tokenId', 'Name', 'Att.Rarity', 'Att.Club', 'Att.Competition', 'Att.Position', 'Att.Serial_number', 'lastSale.buyer']
-dfcols = df[cols]
-top30_Sell = (dfcols.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False)).head(30)
+cols = [ 'Name', 'Att.Rarity','blockchain','lastSale.price', 'lastSale.CurrentUSDPrice', 'lastSale.date','tokenId', 'Att.Club', 'Att.Competition', 'Att.Position', 'Att.Serial_number', 'lastSale.buyer']
+df = dfData[cols]
+top30_Sell = (df.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False))
 
 st.write(top30_Sell)
