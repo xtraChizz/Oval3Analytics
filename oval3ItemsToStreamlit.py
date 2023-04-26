82a7f40431751189825e20153f4532c4044f76d2
Noyde <noyde@hotmail.com>
2023-04-26 22:33:25 +0200

    test2

--- oval3ItemsToStreamlit.py
+++ oval3ItemsToStreamlit.py
@@ -19,5 +19,4 @@ def load_data(sheets_url):
 df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+st.write(df)
