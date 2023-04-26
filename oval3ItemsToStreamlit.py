cd23b4fb59b513801718295e1b0101093f722834
Noyde <noyde@hotmail.com>
2023-04-26 22:20:43 +0200

    Initial commit

---
+++ oval3ItemsToStreamlit.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+import requests
+import json
+import streamlit as st
+
+################### Declarations ################
+
+################### Functions ###################
+
+st.write("Hello World!")
