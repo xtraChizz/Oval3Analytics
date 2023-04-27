13071543d2d6c081be35a21d946f117ea996f850
Noyde <noyde@hotmail.com>
2023-04-27 15:52:16 +0200

    top 10

--- oval3ItemsToStreamlit.py
+++ oval3ItemsToStreamlit.py
@@ -1,10 +1,7 @@
-
 import pandas as pd
 import streamlit as st
 
 ################### Declarations ################
-
-
 
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
@@ -13,21 +10,29 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
-
-################### Visualisation ###################
-st.write("Welcome to Oval3 Analytics")
-
-
 dfData = load_data(st.secrets["Oval3DataSource"])
 
-# Print results.
-#st.write(df)
+cols = ['Name','Att.Rarity','blockchain','lastSale.price', 'lastSale.CurrentUSDPrice', 'lastSale.date' ,'tokenId',   'Att.Club', 'Att.Competition', 'Att.Position', 'Att.Serial_number', 'lastSale.buyer']
+df = dfData[cols]
 
+# Remove nan values
+df = df.dropna(subset = ["lastSale.CurrentUSDPrice"]) 
+################### Visualisation ###################
 
-dfData = dfData.dropna(subset = ["lastSale.CurrentUSDPrice"]) 
-dfData['lastSale.CurrentUSDPrice'] = dfData['lastSale.CurrentUSDPrice'].apply(lambda x: float(x.split()[0].replace(',', '.')))
+st.write("Welcome to Oval3 Analytics")
 
-order_byDate = dfData.sort_values(by=['lastSale.date'],ascending=False)
-top10_Sell = dfData.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False).head(30)
-ById = dfData.sort_values(by=['tokenId']).head(40)
-st.write(top10_Sell)
+# Replace ',' by '.' in price colums
+df['lastSale.price'] = df['lastSale.price'].apply(lambda x: float(x.split()[0].replace(',', '.')))
+df['lastSale.CurrentUSDPrice'] = df['lastSale.CurrentUSDPrice'].apply(lambda x: float(x.split()[0].replace(',', '.')))
+
+#Set tokenId as index
+df.set_index('tokenId', inplace=True)
+
+#Filters
+order_byDate = df.sort_values(by=['lastSale.date'],ascending=False)
+top10_Sell = df.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False).head(10)
+ById = df.sort_values(by=['tokenId']).head(40)
+
+# Show top 10 sell
+st.write("Top10 Sell")
+st.write(top10_Sell.style.format(thousands=" "), use_container_width=True)
