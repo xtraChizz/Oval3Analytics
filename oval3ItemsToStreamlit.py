9865237e0726d67136befd14cdcecab6cc7968fe
Noyde <noyde@hotmail.com>
2023-04-27 16:46:54 +0200

    top 10 Sell by rarrity

--- oval3ItemsToStreamlit.py
+++ oval3ItemsToStreamlit.py
@@ -30,9 +30,33 @@ df.set_index('tokenId', inplace=True)
 
 #Filters
 order_byDate = df.sort_values(by=['lastSale.date'],ascending=False)
-top10_Sell = df.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False).head(10)
 ById = df.sort_values(by=['tokenId']).head(40)
 
 # Show top 10 sell
+top10_Sell = df.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False).head(10)
 st.write("Top10 Sell")
 st.write(top10_Sell.style.format(thousands=" "), use_container_width=True)
+
+#top 10 sell UNIQUE
+dfunique = df[df["Att.Rarity"] == 'UNIQUE']
+top10_Sell_Unique = dfunique.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False).head(10)
+st.write("Top10 Sell Unique ")
+st.write(top10_Sell_Unique.style.format(thousands=" "), use_container_width=True)
+
+#top 10 sell SR
+dfSR= df[df["Att.Rarity"] == 'SUPER RARE']
+top10_Sell_SR = dfSR.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False).head(10)
+st.write("Top10 Sell Super Rare ")
+st.write(top10_Sell_SR.style.format(thousands=" "), use_container_width=True)
+
+#top 10 sell Rare
+dfRare= df[df["Att.Rarity"] == 'RARE']
+top10_Sell_Rare = dfRare.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False).head(10)
+st.write("Top10 Sell Rare ")
+st.write(top10_Sell_Rare.style.format(thousands=" "), use_container_width=True)
+
+#top 10 sell Limited
+dfLimited= df[df["Att.Rarity"] == 'LIMITED']
+top10_Sell_Limited = dfLimited.sort_values(by=['lastSale.CurrentUSDPrice'],ascending=False).head(10)
+st.write("Top10 Sell Limited ")
+st.write(top10_Sell_Limited.style.format(thousands=" "), use_container_width=True)
